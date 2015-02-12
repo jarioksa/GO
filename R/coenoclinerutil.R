437db825e542cfcb6a38adba2e520a76aeeed8eb
Jari Oksanen <jari.oksanen@oulu.fi>
2015-02-12 11:30:17 +0200

    Add translation of Gaussian parameters to beta parameter
    Other parameters are similar, but shape (alpha, gamma) varies
    randomly.
    
    The height is now equal, but it should be adjusted so that
    Gaussian and beta have the same integral so that they predict
    the same number of occurrences. Flat beta responses should be lower
    and peaked should be higher than corresponding Gaussian.

--- R/coenoclinerutil.R
+++ R/coenoclinerutil.R
@@ -40,6 +40,43 @@
          py = cbind("opt" = uy, "tol" = ty))
 }
 
+#' @param gausspar Gaussian response parameters for species as
+#' returned by \code{BinomGaussPar}.
+#' @param shape Random uniform range of shape parameters \eqn{alpha}
+#' and \eqn{gamma} of response function
+#' @param range Range of beta response in $t$ (\sQuote{sd}) units of
+#' Gaussian response function.
+#'
+#' @describeIn coenoclinerutil Translate Gaussian parameters into
+#' corresponding beta response parameters.
+#' @export
+`Gauss2betaPar` <-
+    function(gausspar, shape = c(0.5, 6.5), range = 5)
+{
+    ## Define beta response so that it as similar to a Gaussian model
+    ## as possible -- except for shape. Input **must** be similar as
+    ## from BinomGausPar. This is not checked.
+    nsp <- nrow(gausspar[[1]])
+    ## shapes uniform in (shape)
+    ax <- runif(nsp, shape[1], shape[2])
+    gx <- runif(nsp, shape[1], shape[2])
+    ay <- runif(nsp, shape[1], shape[2])
+    gy <- runif(nsp, shape[1], shape[2])
+    ## ranges a multiple of Gaussian tol
+    rx <- range * gausspar$px[,"tol"]
+    ry <- range * gausspar$py[,"tol"]
+    ## modal abundance at Gaussian opt
+    mx <- gausspar$px[,"opt"]
+    my <- gausspar$py[,"opt"]
+    ## Same height as the Gaussian response: should be the same
+    ## integral so that beta and Gaussian predict the same number of
+    ## occurrences
+    A0 <- gausspar$px[,"h"]
+    ## collect
+    list(px = cbind("m" = mx, "r" = rx, "alpha" = ax, "gamma" = gx, "A0" = A0),
+         py = cbind("m" = my, "r" = ry, "alpha" = ay, "gamma" = gy))
+}
+
 #' @param comm Community data.
 #'
 #' @describeIn coenoclinerutil Drop missing species from the data.
